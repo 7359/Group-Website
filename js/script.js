6aa54ac9d2e49d78340ce9c5a17ec255fb9be0db
7359 <bbuchan@stevens.edu>
2017-12-01 18:55:23 -0500

    Implemented script for action
    Several outstanding bugs, but it kinda works. Hid the Dr. Strange poster so cycling actually occurs.

--- js/script.js
+++ js/script.js
@@ -2,11 +2,16 @@
 *Make the scroll button call this and add some additional posters, with the
 *link element's display set to "none" at first.
 */
-function rollImages(){
+function rollImages(button){
     //May behave oddly if not all initial active items are consecutive
-    var source=this.parentElement;
-    var images=source.childNodes;
-    images.pop();//Remove the scroll button itself
+    var source=button.parentElement;
+    var children=source.childNodes;
+    var images=[];
+    for(var i=0; i<children.length; i++){
+	if(children[i].nodeName=="A"){
+	    images.push(children[i]);
+	}
+    }
     var i=0;
     while(images[i].style.display!="none"){
 	//Don't want to be in the middle of a hanging bar
