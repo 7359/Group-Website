590589c19cf78fc0f9a4ea6de8951ee218ac950e
Joaquin <jtalon@stevens.edu>
2017-12-04 21:44:47 -0500

    minor

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,17 @@
 function nameValidation() {
-    var x = document.forms["main"]["fname"].value;
-    if (x == "") {
+    if (document.forms["main"]["name"].value == "") {
         alert("Please fill out your first name.");
         return false;
     }
+   if(document.forms["main"]["comment"].value == ""){
+       alert("Tell us what you have to say!");
+       return false;
+   }
+    var thanks = document.createElement("H1");
+    thanks.style.textAlign = "center";
+    thanks.style.verticalAlign = "center";
+    thanks.innerHTML = "Thank you for your feedback.<br><br>We appreciate your support.";
+    
+    document.getElementById("contact").innerHTML = "<br><br><br>";
+    document.getElementById("contact").appendChild(thanks);
 }
