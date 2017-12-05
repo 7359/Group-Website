e940c405889c16876dc39d6b44a5938b1f7e3858
Joaquin <jtalon@stevens.edu>
2017-12-05 01:50:10 -0500

    added data usage in contact.js

--- js/contact.js
+++ js/contact.js
@@ -11,9 +11,29 @@ function nameValidation() {
     var thanks = document.createElement("H1");
     thanks.style.textAlign = "center";
     thanks.style.verticalAlign = "center";
-    thanks.innerHTML = "Thank you for your feedback.<br><hr>We appreciate your support.";
-    var feedback = document.createElements("DIV");
-    feedback.style.color = "red";
+    var thanksHTML = "";
+    thanks.innerHTML = "Thank you for your feedback.<br><hr>";
+    var feedback = document.createElement("DIV");
+    var HTMLstring = "";
+    var rating = "";
+    var radioButtons = document.getElementsByName("Rating");
+    for(var i=0;i<radioButtons.length;i++){
+        if(radioButtons[i].checked == true){
+            rating = radioButtons[i].value;
+        }
+    }
+    HTMLstring += "<h3>"+document.getElementById("name").value;
+    if(document.getElementById("genre").value != "What's your favorite genre?"){
+        HTMLstring += ", whose favorite genre is "+document.getElementById("genre").value + ",";
+    }
+    HTMLstring += " says: </h3>" + document.getElementById("comment").value;
+    HTMLstring += "<h1>"+rating+" out of 10</h1>";
+    feedback.style.fontSize = "26px";
+    feedback.innerHTML = HTMLstring;
+    feedback.style.color = "black";
+    thanks.appendChild(feedback);
+    thanks.innerHTML += "<hr>We appreciate your support.";
     document.getElementById("contact").innerHTML = "<br><br><br>";
     document.getElementById("contact").appendChild(thanks);
+   
 }
